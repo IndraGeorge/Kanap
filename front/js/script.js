2cee3be5d7b2a706bc7a2dc44273f600a7d1fc23
IndraGeorge <george.indraa@gmail.com>
2022-10-03 10:32:42 +0200

    page d'accueil terminée

--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,44 @@
+// call API with Fetch
+
+let products = [];
+
+async function loadingPageHome() {
+   await fetch("http://localhost:3000/api/products ")
+   .then((res) => res.json())
+    .then((data) => {
+      products = data;        
+      return products
+    }
+    )
+
+    .catch ((err) => {
+      console.log("fetch err")
+      // une erreur est survenue
+      document.getElementById("items").innerHTML += "Nos produits sont indisponibles pour le moment"
+    })
+}
+
+
 // home page containing API products
-let product = [];
+
+const productsDisplay = async () => {
+  await loadingPageHome();
+
+for (let listProduct of products) {
+  document
+    .getElementById("items")
+    .innerHTML += `<a href="./product.html?id=${listProduct._id}">
+          <article>
+            <img src="${listProduct.imageUrl}" alt="${listProduct.altTxt}">
+            <h3 class="productName">${listProduct.name}</h3>
+            <p class="productDescription">${listProduct.description}</p>
+          </article>
+        </a>`
+ }
+
+}
 
 
-  fetch("http://localhost:3000/api/products")
-    .then(res => res.json())
-    .then(data => {
-        product = data;
-        for (let listProduct of product) {
-            document
-                .getElementById("items")
-                .innerHTML += `<a href="./product.html?id=${listProduct._id}">
-                <article>
-                  <img src="${listProduct.imageUrl}" alt="${listProduct.altTxt}">
-                  <h3 class="productName">${listProduct.name}</h3>
-                  <p class="productDescription">${listProduct.description}</p>
-                </article>
-              </a>` 
-         }
-    });
-    
-
-
+productsDisplay();
 
 
