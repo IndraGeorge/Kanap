e2f429b13a19952d8869552cf957408663eb08cb
IndraGeorge <george.indraa@gmail.com>
2022-10-04 20:37:35 +0200

    ajout panier en cours

--- front/js/product.js
+++ front/js/product.js
@@ -1,36 +1,75 @@
 // Url SearchParams
+const id = new URLSearchParams(window.location.search).get("id");
+console.log(id);
 
-let pageProducts = "http://127.0.0.1:5500/front/html/product.html?id=";
-let url = new URL(pageProducts);
-let id = url.searchParams.get("id");
-console.log(id)
-
+// array product
 let productData = [];
 
-// fetch product 
+// button add to cart
+let button = document.getElementById("addToCart");
 
+
+// fetch product 
 const loadingPageProduct = async () => {
-    await fetch("http://localhost:3000/api/products/" + id)
+    await fetch(`http://localhost:3000/api/products/${id}`)
         .then((res) => res.json())
         .then((data) => {
             productData = data
-            console.log(productData)
+            return productData
         })
 }
 
 
 // page product containing details of product
-
 const productDisplay = async () => {
     await loadingPageProduct();
-  
-        document
-            .querySelector(".item__img")
-            .innerHTML += `<img src="${productData.imageUrl}" alt="${productData.altTxt}"></img>`
-    
 
-}
+    document.title = productData.name
+
+    document
+        .querySelector(".item__img")
+        .innerHTML += `<img src="${productData.imageUrl}" alt="${productData.altTxt}"></img>`
+
+    document
+        .getElementById("title")
+        .textContent += productData.name
+
+    document
+        .getElementById("price")
+        .textContent += productData.price
+
+    document
+        .getElementById("description")
+        .textContent += productData.description
+
+    // boucle colors
+    for (let productColors of productData.colors) {
+        document
+            .getElementById("colors")
+            .innerHTML += `<option value="${productColors}">${productColors}</option>`
+        console.log(productColors)
+
+    }
+
+    addBasket(productData);
+};
+
 
 productDisplay();
 
+// add to cart 
 
+const addBasket = () => {
+
+    button.addEventListener('click', function () {
+
+        let basket = JSON.parse(localStorage.getItem("kanap")); 
+        localStorage.setItem("kanap", JSON.stringify(basket)); 
+        console.log(basket);
+        basket.push(productData)
+
+       
+
+    })
+
+} 
