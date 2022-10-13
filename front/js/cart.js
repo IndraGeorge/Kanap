512e760002462dd2b2dc62018d6e75b3dfa59c76
IndraGeorge <george.indraa@gmail.com>
2022-10-13 20:06:01 +0200

    modification panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,75 +1,76 @@
 let cart = JSON.parse(localStorage.getItem("basketClient"))
 
 // condition if there is nothing in local storage
-if (cart == null) {
 
-  console.log("Votre panier est vide")
+const productsInLocalStorage = () => {
 
-} else {
+  if (cart == null) {
 
-  cart
+    console.log("Votre panier est vide")
 
-  for (i = 0; i < cart.length; i++) {
+  } else {
 
-    let id = cart[i]._id;
-    let quantity = cart[i].quantity;
-    let colors = cart[i].colors;
+    for (i = 0; i < cart.length; i++) {
 
-    let urlProducts = `http://localhost:3000/api/products/${id}`
-    fetch(urlProducts)
-      .then((res) => res.json())
-      .then((data) => {
-
-        let image = data.imageUrl
-        let name = data.name
-        let price = data.price * quantity
-
-        let items = document.getElementById("cart__items");
-
-        items.innerHTML +=
-          `<article class="cart__item" data-id="${id}" data-color="${colors}">
-                <div class="cart__item__img">
-                  <img src="${image}" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${name}</h2>
-                    <p>${colors}</p>
-                    <p>${price}€</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article>`
+      let id = cart[i]._id;
+      let quantity = cart[i].quantity;
+      let colors = cart[i].colors;
 
 
-      })
+      fetch(`http://localhost:3000/api/products/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          
+          let image = data.imageUrl
+          let name = data.name
+          let price = data.price * quantity
 
-      .catch((err) => {
-        console.log("fetch err")
-      })
+          let items = document.querySelector("#cart__items")
+          //Create article
+          items.innerHTML += `<article class="cart__item" data-id="${id}" data-color="${colors}">
+          <div class="cart__item__img">
+            <img src="${image}" alt="Photographie d'un canapé">
+          </div>
+          <div class="cart__item__content">
+            <div class="cart__item__content__description">
+              <h2>${name}</h2>
+              <p>${colors}</p>
+              <p>${price}€</p>
+            </div>
+            <div class="cart__item__content__settings">
+              <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
+              </div>
+              <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+              </div>
+            </div>
+          </div>
+        </article>`
+
+          plusQuantity(data);
+          
+        })
+
+    }
 
   }
 
 }
 
-// total quantity
+productsInLocalStorage()
+
+
+//total quantity
+
 let totalQuantity = document.getElementById("totalQuantity")
 
 const totalProducts = cart.reduce((acc, val) => acc + val.quantity, 0)
 
 totalQuantity.textContent = totalProducts
 
-
-// total price
-let totalPrice = document.getElementById("totalPrice")
+// total price 
 
 let total = 0;
 
@@ -83,18 +84,68 @@ for (i = 0; i < cart.length; i++) {
     .then((data) => {
 
       let price = data.price
-        
-          productsPrice = quantity * price
-          total += productsPrice 
-          let totalPrice = document.getElementById("totalPrice")
-          totalPrice.textContent = total
-         
+
+      productsPrice = quantity * price
+      total += productsPrice
+      let totalPrice = document.getElementById("totalPrice")
+      totalPrice.textContent = total
+
     })
 
 }
 
-// increase and reduce quantity
+// Increase and reduce products
 
-let itemQuantity = document.querySelector(".cart__item")
-console.log(itemQuantity)
+function plusQuantity() {
+
+  let input = document.querySelectorAll(".itemQuantity")
+
+  input.forEach((changeQuantity) => {
+
+    changeQuantity.addEventListener("change", (e) => {
+
+      inputQuantity = Number(changeQuantity.value);
+      console.log(inputQuantity);
+
+      let article = changeQuantity.closest("article");
+
+      console.log(article);
+
+      for (i = 0; i < cart.length; i++) {
+        let id = cart[i]._id
+        let colors = cart[i].colors
+        let quantity = cart[i].quantity
+
+        if (id == article.dataset.id && colors == article.dataset.color) {
+
+            quantity = inputQuantity
+            console.log("ajout panier"),
+            localStorage.setItem("basketClient", JSON.stringify(cart)),
+            document.querySelectorAll(".itemQuantity")[i].textContent = quantity,
+            document.querySelectorAll("#totalPrice")[i].textContent = quantity * data.price
+            changeTotalQuantity()
+          
+        }
+
+      }
+
+    })
+  })
+
+}
+
+// New total quantity
+
+function changeTotalQuantity() {
+
+  let totalQuantity = document.getElementById("totalQuantity")
+
+  const totalProducts = cart.reduce((acc, val) => acc + val.quantity, 0)
+
+  totalQuantity.textContent = totalProducts
+
+}
+
+
+
 
